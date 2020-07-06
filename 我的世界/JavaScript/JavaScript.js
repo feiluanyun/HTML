8a4139d725b653befc8c53ffe69baab46d8ccf95
feiluanyun <56372015+feiluanyun@users.noreply.github.com>
2020-07-06 11:22:40 +0800

    Update JavaScript.js

--- 我的世界/JavaScript/JavaScript.js
+++ 我的世界/JavaScript/JavaScript.js
@@ -1,17 +1,17 @@
 
 document.getElementById("sy").onclick=function(){
-	window.location="风光欣赏网站 - 李哲洋（首页）.html";
+	window.location=".html";
 }
 
 document.getElementById("xs").onclick=function(){
-	window.location="风光欣赏网站 - 李哲洋（欣赏）.html";
+	window.location=".html";
 }
 
 document.getElementById("bj").onclick=function(){
-	window.location="风光欣赏网站 - 李哲洋（背景）.html";
+	window.location=".html";
 }
 
 document.getElementById("zc").onclick=function(){
-	window.location="风光欣赏网站 - 李哲洋（登陆）.html";
+	window.location=".html";
 }
 
